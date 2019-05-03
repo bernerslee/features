ab35415802984ff6134d2ea93005569d5c0e2514
Jessica <jessica.herrera82094@gmail.com>
2019-05-02 21:52:08 -0700

    updated host ip for database -- ran into connection error

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// let host = '3.16.31.9';
-let host = 'ec2-3-16-31-9.us-east-2.compute.amazonaws.com';
+ let host = '3.17.67.209';
+// let host = 'ec2-3-16-31-9.us-east-2.compute.amazonaws.com';
 let port = 3306;
 let user = 'student';
 let password = 'password';
